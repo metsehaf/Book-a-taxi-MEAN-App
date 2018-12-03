ee7bd4cc609726a0ec6de38cf74b8d969f1f4468
Girum Hagos <hagosgirum@gmail.com>
2018-12-02 19:22:44 -0500

    Removed Security KEY for PUSHER

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
   pusher: {
-    key: '2047f03df71155bff833'
+    key: 'PUSHER_KEY'
 },
   apiUrl: 'http://localhost:3000/api'
-  // apiUrl: 'http://sharptaxibooking-env.wsckjmx2ax.us-east-2.elasticbeanstalk.com/api'
 };
